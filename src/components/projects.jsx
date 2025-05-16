0a2760231bdf1fb6605ee97dd30dfc9c609abe8f
jovinroshanj <jovin.roshan.dev@gmail.com>
2025-05-16 19:00:53 +0530

    add final all project page code'

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import SocialMediaBtn from "./social_media_btn"
 import BuilkMailVideo from "../assets/project_image/bulk-mail-application.mp4"
+import QuizAppImage from "../assets/project_image/QuizApp.mp4"
 import WeatherAppVideo from "../assets/project_image/Weather_React_Application.mp4"
 import ToDoListVideo from "../assets/project_image/ToDoListApp.mp4"
 import ReactLoginAppVideo from "../assets/project_image/login_page_react_application.mp4"
@@ -31,45 +32,52 @@ export default function Project({ setActiveBtn }) {
             Video:WeatherAppVideo
         },
         {
-            projectName:"TODO List React APP",
-            description:"A modern and responsive weather application built with React.js, designed to provide real-time weather updates based on the user's current location or manual city search. The app fetches accurate weather data including temperature, weather conditions, humidity, and wind speed using a third-party weather API.",
-            githubLink:"https://github.com/Jovinroshandev/WeatherApp",
+            projectName:"TODO Task Manager",
+            description:"A simple and responsive to-do list app to manage daily tasks. Built with React, Tailwind CSS, JavaScript, and CSS, it lets users easily add, edit, and delete tasks. The clean design and user-friendly interface make task management quick and efficient.",
+            githubLink:"https://github.com/Jovinroshandev/taskManager",
             demoLink:"https://task-manager-brown-theta.vercel.app/",
             Video:ToDoListVideo
         },
         {
+            projectName:"Quiz App",
+            description:"An interactive quiz application built with React, Tailwind CSS, JavaScript, and CSS. Quizee lets users test their knowledge with multiple-choice questions. It features a clean UI, instant feedback on answers, and smooth navigation between questions. Fully responsive and easy to use on any device",
+            githubLink:"https://github.com/Jovinroshandev/QuizeeApp",
+            demoLink:"https://quizee-app-seven.vercel.app/",
+            Video:QuizAppImage
+        },
+        {
             projectName:"React Login APP",
             description:"A modern and responsive weather application built with React.js, designed to provide real-time weather updates based on the user's current location or manual city search. The app fetches accurate weather data including temperature, weather conditions, humidity, and wind speed using a third-party weather API.",
-            githubLink:"https://github.com/Jovinroshandev/WeatherApp",
-            demoLink:"https://task-manager-brown-theta.vercel.app/",
+            githubLink:"https://github.com/Jovinroshandev/loginapp_frontend",
+            demoLink:"https://loginapp-frontend-ruddy.vercel.app/",
             Video:ReactLoginAppVideo
         },
         {
             projectName:"Redom Number Guss Game",
-            description:"A modern and responsive weather application built with React.js, designed to provide real-time weather updates based on the user's current location or manual city search. The app fetches accurate weather data including temperature, weather conditions, humidity, and wind speed using a third-party weather API.",
-            githubLink:"https://github.com/Jovinroshandev/WeatherApp",
-            demoLink:"https://task-manager-brown-theta.vercel.app/",
+            description:"A fun and interactive number guessing game built with HTML, CSS, and JavaScript. The app generates a random number, and the user tries to guess it with feedback provided after each attempt. It’s a simple game that improves logical thinking and adds a playful touch to the portfolio. Fully responsive and easy to play on any device.",
+            githubLink:"https://github.com/Jovinroshandev/Game-FindNumber",
+            demoLink:"https://game-find-number-orpin.vercel.app/",
             Video:NumberGussingVideo
         },
         {
             projectName:"Perfume Shop Website",
-            description:"A modern and responsive weather application built with React.js, designed to provide real-time weather updates based on the user's current location or manual city search. The app fetches accurate weather data including temperature, weather conditions, humidity, and wind speed using a third-party weather API.",
-            githubLink:"https://github.com/Jovinroshandev/WeatherApp",
-            demoLink:"https://task-manager-brown-theta.vercel.app/",
+            description:"A stylish and responsive perfume store website built with React, Tailwind CSS, JavaScript, and CSS. Perfumy features a modern layout showcasing perfume products with smooth scrolling, clean visuals, and a user-friendly interface. Ideal for showcasing e-commerce design skills with a focus on aesthetics and responsiveness.",
+            githubLink:"https://github.com/Jovinroshandev/Perfumy-React",
+            demoLink:"https://perfumy-react-application.vercel.app/",
             Video:PerfumyReactAppVideo
         },
         {
             projectName:"Dog Shop Website",
-            description:"A modern and responsive weather application built with React.js, designed to provide real-time weather updates based on the user's current location or manual city search. The app fetches accurate weather data including temperature, weather conditions, humidity, and wind speed using a third-party weather API.",
-            githubLink:"https://github.com/Jovinroshandev/WeatherApp",
-            demoLink:"https://task-manager-brown-theta.vercel.app/",
+            description:"A clean and responsive dog shop website built with HTML, CSS, and JavaScript. Love Tails showcases dog products with a modern design, smooth layout, and user-friendly experience. Perfect for displaying frontend development and UI design skills, especially for pet-related e-commerce websites.",
+            githubLink:"https://github.com/Jovinroshandev/Love_Tails-Greenden",
+            demoLink:"https://love-tails-greenden.vercel.app/",
             Video:LoveTailVideo
         },
         {
             projectName:"Trip Advisor Website",
-            description:"A modern and responsive weather application built with React.js, designed to provide real-time weather updates based on the user's current location or manual city search. The app fetches accurate weather data including temperature, weather conditions, humidity, and wind speed using a third-party weather API.",
-            githubLink:"https://github.com/Jovinroshandev/WeatherApp",
-            demoLink:"https://task-manager-brown-theta.vercel.app/",
+            description:"A responsive clone of TripAdvisor built with HTML, CSS, and JavaScript. This project features a clean layout showcasing destinations, hotels, and restaurants, along with review and rating sections. It highlights frontend development skills with a focus on layout design, content organization, and mobile-friendly responsiveness.",
+            githubLink:"https://github.com/Jovinroshandev/Trip-Advisor",
+            demoLink:"https://trip-advisor-rust.vercel.app/",
             Video:TripAdvisorVideo
         },
     ]
@@ -77,13 +85,13 @@ export default function Project({ setActiveBtn }) {
     const ProjectCard = (props) => {
         const {projectName,description,githubLink,demoLink,Video} = props
         return (
-            <div className="bg-gray-100 border w-fit h-96 p-2 rounded-2xl md:flex md:flex-col md:justify-between">
+            <div className="bg-gray-100 border w-fit h-96 p-2 rounded-2xl flex flex-col justify-between">
                 <h1 className="text-center text-lg my-1 font-medium text-red-500">{projectName}</h1>
                 <video className="w-60 h-fit rounded-lg" src={Video} autoPlay muted></video>
                 <p className="text-wrap w-60 text-xs text-justify">{description}</p>
                 <div className="mt-5 flex justify-around">
-                    <a href={demoLink} target="_blank" className="bg-red-500 rounded-full px-5 py-1 text-white font-bold"><i class="fa-solid fa-globe"></i> Demo</a>
-                    <a href={githubLink} target="_blank" className="bg-black rounded-full px-5 py-1 text-white font-bold"><i class="fa-brands fa-github"/> GitHub</a>
+                    <a href={demoLink} target="_blank" className="bg-red-500 rounded-full px-5 py-1 text-white font-bold text-xs md:text-sm"><i class="fa-solid fa-globe"></i> Live Demo</a>
+                    <a href={githubLink} target="_blank" className="bg-black rounded-full px-5 py-1 text-white font-bold text-xs md:text-sm"><i class="fa-brands fa-github"/> GitHub</a>
                 </div>
             </div>
         )
